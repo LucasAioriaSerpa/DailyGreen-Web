f6125191090f910d1f40bcaa4adfba1584a2e028
Lucas Aioria Serpa <136469417+Garudaakuma@users.noreply.github.com>
2025-03-26 12:16:14 -0300

    fix: mudança de nomes do modelo logico do MySQL

--- DATABASE/setup.sql
+++ DATABASE/setup.sql
@@ -1,11 +1,4 @@
 
 CREATE DATABASE db_dailygreen;
 USE db_dailygreen;
---? users
-CREATE TABLE participante (
-    id INT PRIMARY KEY AUTOINCREMENT NOT NULL,
-    nome VARCHAR(100) NOT NULL,
-    email VARCHAR(100) UNIQUE NOT NULL,
-    senha VARCHAR(100) NOT NULL
-)
-CREATE TABLE 
+
