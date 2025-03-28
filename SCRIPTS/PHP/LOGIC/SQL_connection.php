b2b32ed6d08daad3397ac6e05af5d93d5e0df2da
Lucas Aioria Serpa <136469417+LucasAioriaSerpa@users.noreply.github.com>
2025-03-28 00:26:07 -0300

    class sql created

--- SCRIPTS/PHP/LOGIC/SQL_connection.php
+++ SCRIPTS/PHP/LOGIC/SQL_connection.php
@@ -1,24 +1,26 @@
 
 <?php
-$SQLdata = json_decode(file_get_contents("/xampp/htdocs/DailyGreen-Project/JSON/bd_info.json"), true);
-$server_info = [
-    "servername"=>"{$SQLdata["servername"]}",
-    "username"=>"{$SQLdata["username"]}",
-    "password"=>"{$SQLdata["password"]}",
-    "database"=>"{$SQLdata["database"]}",
-    "port"=>"{$SQLdata["port"]}"
-];
+class SQL_connection {
+    private array $SQLdata = json_decode(file_get_contents("/xampp/htdocs/DailyGreen-Project/JSON/bd_info.json"), true);
+    private array $server_info = [
+        "servername"=>"{$SQLdata["servername"]}",
+        "username"=>"{$SQLdata["username"]}",
+        "password"=>"{$SQLdata["password"]}",
+        "database"=>"{$SQLdata["database"]}",
+        "port"=>"{$SQLdata["port"]}"
+    ];
+    function try_connect() {
+        if ($this->server_info["password"] === "") {
+            header("Location: /DailyGreen-Project/SCRIPT/PHP/SQL_connection_error.php");
+        }
+        $conn = new mysqli( $this->server_info['servername'],
+                            $this->server_info['username'],
+                            $this->server_info['password'],
+                            $this->server_info['database'],
+                                $this->server_info['port']);
+        if ($conn->connect_error) {
+            header("Connection Error: " . $conn->connect_error);
+        }
+    }
 
-if ($server_info["password"] === "") {
-    header("Location: /DailyGreen-Project/SCRIPT/PHP/SQL_connection_error.php");
-}
-
-$conn = new mysqli( $server_info['servername'],
-                    $server_info['username'],
-                    $server_info['password'],
-                    $server_info['database'],
-                        $server_info['port']);
-
-if ($conn->connect_error) {
-    header("Connection Error: " . $conn->connect_error);
 }
