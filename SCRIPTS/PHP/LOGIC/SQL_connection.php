17d86397e983b04a8364d0aa69a8c3eb7ae827d5
Lucas Aioria Serpa <136469417+LucasAioriaSerpa@users.noreply.github.com>
2025-03-28 08:13:37 -0300

    sql forms connection error

--- SCRIPTS/PHP/LOGIC/SQL_connection.php
+++ SCRIPTS/PHP/LOGIC/SQL_connection.php
@@ -1,25 +1,25 @@
 
 <?php
 class SQL_connection {
-    private array $SQLdata = json_decode(file_get_contents("/xampp/htdocs/DailyGreen-Project/JSON/bd_info.json"), true);
-    private array $server_info = [
-        "servername"=>"{$SQLdata["servername"]}",
-        "username"=>"{$SQLdata["username"]}",
-        "password"=>"{$SQLdata["password"]}",
-        "database"=>"{$SQLdata["database"]}",
-        "port"=>"{$SQLdata["port"]}"
+    private array $sqlData = json_decode(file_get_contents("/xampp/htdocs/DailyGreen-Project/JSON/bd_info.json"), true);
+    private array $serverInfo = [
+        "servername"=>"{$this->sqlData["servername"]}",
+        "username"=>"{$this->sqlData["username"]}",
+        "password"=>"{$this->sqlData["password"]}",
+        "database"=>"{$this->sqlData["database"]}",
+        "port"=>"{$this->sqlData["port"]}"
     ];
-    function try_connect() {
-        if ($this->server_info["password"] === "") {
+    private mysqli $conn = new mysqli(  $this->serverInfo['servername'],
+                                        $this->serverInfo['username'],
+                                        $this->serverInfo['password'],
+                                        $this->serverInfo['database'],
+                                        $this->serverInfo['port']);
+    public function connect() {
+        if ($this->serverInfo["password"] === "") {
             header("Location: /DailyGreen-Project/SCRIPT/PHP/SQL_connection_error.php");
         }
-        $conn = new mysqli( $this->server_info['servername'],
-                            $this->server_info['username'],
-                            $this->server_info['password'],
-                            $this->server_info['database'],
-                                $this->server_info['port']);
-        if ($conn->connect_error) {
-            header("Connection Error: " . $conn->connect_error);
+        if ($this->conn->connect_error) {
+            header("Connection Error: " . $this->conn->connect_error);
         }
     }
 
---
+++ SCRIPTS/PHP/SQL_connection_error.php
@@ -0,0 +1,31 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        <h1>✖ Falha na conexão! ✖</h1>
+        <div class="forms">
+        <form id="formBD" name="formBD" action="" method="post">
+        <div class="forms-inputs">
+            <label for="servername">Server name:</label>
+            <input type="text" id="servername" name="servername" required>
+            <label for="username">User name:</label>
+            <input type="text" id="username" name="username" required>
+            <label for="password">Password:</label>
+            <input type="password" id="password" name="password" required>
+            <label for="database">Database:</label>
+            <input type="text" id="database" name="database" required>
+            <label for="port">Port:</label>
+            <input type="text" id="port" name="port" required>
+            <input type="submit" value="Conectar">
+        </div>
+        </form>
+        </div>
+    </header>
+</body>
+</html>
