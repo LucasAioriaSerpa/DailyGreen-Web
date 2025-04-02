a6fdfd9b74c366ad492e024b486ee2a3615588c3
Lucas Aioria Serpa <136469417+LucasAioriaSerpa@users.noreply.github.com>
2025-04-01 21:07:12 -0300

    fix: cypher decoder working finally

--- SCRIPTS/PHP/LOGIC/Cypher.php
+++ SCRIPTS/PHP/LOGIC/Cypher.php
@@ -10,8 +10,9 @@ class EncodeDecode {
     public function encrypt(string $data) {
         $key = base64_decode(json_decode(file_get_contents("/xampp/htdocs/DailyGreen-Project/JSON/cypher_data.json"), true)["key"]);
         $iv = openssl_random_pseudo_bytes($this->iv_length);
-        $encryptedData = hash_hmac('sha3-512',openssl_encrypt($data,$this->method,$key, OPENSSL_RAW_DATA, $iv), $key, TRUE);
-        $output = base64_encode($iv.$encryptedData);
+        $encryptedData = openssl_encrypt($data,$this->method,$key, OPENSSL_RAW_DATA, $iv);
+        $encryptedData_hashMask = hash_hmac('sha3-512',$encryptedData, $key, TRUE);
+        $output = base64_encode($iv.$encryptedData_hashMask.$encryptedData);
         return $output;
     }
     public function decrypt(string $input) {
@@ -19,9 +20,11 @@ class EncodeDecode {
         $key = base64_decode(json_decode(file_get_contents("/xampp/htdocs/DailyGreen-Project/JSON/cypher_data.json"), true)["key"]);
         $mix = base64_decode($input);
         $iv = substr($mix,0,$this->iv_length);
-        $substr_encrypt = substr($mix,$this->iv_length);
-        $data = hash_hmac('sha3-512',openssl_decrypt($substr_encrypt,$this->method,$key,OPENSSL_RAW_DATA,$iv),$key, TRUE);
-        if (hash_equals($substr_encrypt,$data)) {return $data;}
+        $hashMask_encrypt = substr($mix,$this->iv_length,64);
+        $encrypt = substr($mix,$this->iv_length+64);
+        $data = openssl_decrypt($encrypt,$this->method,$key, OPENSSL_RAW_DATA,$iv);
+        $data_hashMask = hash_hmac('sha3-512',$encrypt,$key,TRUE);
+        if (hash_equals($hashMask_encrypt,$data_hashMask)) {return $data;}
         echo "something wrong happened!";
         return false;
     }
--- SCRIPTS/PHP/LOGIC/update_json_SQL_BD.php
+++ SCRIPTS/PHP/LOGIC/update_json_SQL_BD.php
@@ -10,10 +10,10 @@ if (sizeof($_POST) === 0) {
 }
 $jsonDecodedBD['mySql'] = $_POST; //* UPDATES DECODED DATA
 // TODO: keeps return a string length of 64 and don't separate correctly "IV" and encryption = _ =) god please help
-// debug_var($jsonDecodedBD['mySql']["password"]);
-// debug_array($jsonDecodedBD);
-// $jsonDecodedBD['mySql']["password"] = $objEncode->encrypt($jsonDecodedBD['mySql']["password"]);
-// debug_array($jsonDecodedBD);
-// debug_var($objEncode->decrypt($jsonDecodedBD['mySql']["password"]));
+debug_var($jsonDecodedBD['mySql']["password"]);
+debug_array($jsonDecodedBD);
+$jsonDecodedBD['mySql']["password"] = $objEncode->encrypt($jsonDecodedBD['mySql']["password"]);
+debug_array($jsonDecodedBD);
+debug_var($objEncode->decrypt($jsonDecodedBD['mySql']["password"]));
 file_put_contents("/xampp/htdocs/DailyGreen-Project/JSON/bd_info.json",json_encode($jsonDecodedBD, JSON_PRETTY_PRINT));
 #header("Location: SQL_connection.php");
