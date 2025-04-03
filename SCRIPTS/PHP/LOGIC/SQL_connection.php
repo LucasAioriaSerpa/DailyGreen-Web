ef575fdcec01eaa93a257d52de3fbeb6ab60c38d
Lucas Aioria Serpa <136469417+LucasAioriaSerpa@users.noreply.github.com>
2025-04-03 11:36:31 -0300

    feat: creation of the logic of save inputs in json

--- SCRIPTS/PHP/LOGIC/SQL_connection.php
+++ SCRIPTS/PHP/LOGIC/SQL_connection.php
@@ -1,6 +1,8 @@
 
 <?php
-class SQL_connection {
+include "Cypher.php";
+class SQLconnection {
+    private EncodeDecode $decode = new EncodeDecode();
     private array $sqlData = json_decode(file_get_contents("/xampp/htdocs/DailyGreen-Project/JSON/bd_info.json"), true);
     private array $serverInfo = [
         "servername"=>"{$this->sqlData["servername"]}",
@@ -9,17 +11,26 @@ class SQL_connection {
         "database"=>"{$this->sqlData["database"]}",
         "port"=>"{$this->sqlData["port"]}"
     ];
-    public function testConnect() {
+    public function tryConnectBD() {
         $conn = new mysqli( $this->serverInfo['servername'],
                             $this->serverInfo['username'],
-                            $this->serverInfo['password'],
+                            $this->decode->decrypt($this->serverInfo['password']),
                             $this->serverInfo['database'],
                             $this->serverInfo['port']);
         if ($conn->connect_error) {
             header("Location: /DailyGreen-Project/SCRIPT/PHP/SQL_connection_error.php");
         }
+        return $conn;
     }
-    public function addNewParticipant(array $arrayDATA) {
-        
+    public function insertQueryBD(string $query) {
+        $conn = $this->tryConnectBD();
+        if ($conn->query($query) === true) {
+            $last_id = $conn->insert_id;
+            echo "New recorded created sucessfully. Last ID: " . $last_id;
+            return $last_id;
+        } else {
+            echo "Error: " . $query . "<br>" . $conn->error;
+            return false;
+        }
     }
 }
