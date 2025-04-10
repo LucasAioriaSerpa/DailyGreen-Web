30a3095ba117f8959b235a5644a667ffce1a27ae
Flavia Cristina Fagundes <flaviacrisfagundes@gmail.com>
2025-04-10 10:48:14 -0300

    teste com os profs

--- SCRIPTS/PHP/admPage.php
+++ SCRIPTS/PHP/admPage.php
@@ -5,11 +5,18 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="/DailyGreen-Project/SCRIPTS/CSS/style_pagina_administrador.css">
     <link rel="stylesheet" href="/DailyGreen-Project/SCRIPTS/JS/pagina_administrador.js">
-    <title>Adm DailyGreen</title>
+    <title>Adm | DailyGreen</title>
 </head>
 <body>
     <div class="container-admPage">
         <!-- -->
+        <div class="sidebar_esquerda">
+
+        </div>
+
+        <div class="menu_principal">
+
+        </div>
 
     </div>
 </body>
