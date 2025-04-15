12fd8776fcbd809e98ddad5921b6b3d7181f6f1d
Flavia Cristina Fagundes <flaviacrisfagundes@gmail.com>
2025-04-15 10:42:12 -0300

    hotfix: adicionando condição de erro no login do administrador

--- SCRIPTS/PHP/LOGIC/loginVerifyAdm.php
+++ SCRIPTS/PHP/LOGIC/loginVerifyAdm.php
@@ -2,6 +2,7 @@
 <?php
 include_once 'SQL_connection.php';
 include_once 'Cypher.php';
+
 $decode = new EncodeDecode();
 $sqlConnection = new SQLconnection();
 $loginTable = $sqlConnection->callTableBD("administrador", true);
@@ -9,10 +10,12 @@ $loginTable = $sqlConnection->callTableBD("administrador", true);
 foreach ($loginTable as $data) {
     if ($data["email"] == $_POST["email"]) {
         if ($decode->decrypt($data["password"]) == $_POST["password"]) {
+            $loginAdmArray = ["find"=>true, $data];
+            file_put_contents("/xampp/htdocs/DailyGreen-Project/JSON/loginAdm.json",json_encode($loginAdmArray, JSON_PRETTY_PRINT));
             header("Location: /DailyGreen-Project/SCRIPTS/PHP/admPage.php");
             exit();
         }
     }
 }
-echo "false";
-header("Location: /DailyGreen-Project/SCRIPTS/HTML/tela_de_login_adm.html");
+file_put_contents('/xampp/htdocs/DailyGreen-Project/JSON/loginAdm.json',json_encode(['find'=>false],JSON_PRETTY_PRINT));
+header("Location: /DailyGreen-Project/SCRIPTS/PHP/loginAdm.php");
