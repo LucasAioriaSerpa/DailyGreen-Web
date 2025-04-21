f356de075b4a4b7cbb88bc924435203d107c2ac7
Lucas Aioria Serpa <136469417+LucasAioriaSerpa@users.noreply.github.com>
2025-04-21 18:03:59 -0300

    feat: adm can now see list of users created

--- SCRIPTS/PHP/admPage.php
+++ SCRIPTS/PHP/admPage.php
@@ -1,9 +1,10 @@
 
 <?php
     include_once '/xampp/htdocs/DailyGreen-Project/SCRIPTS/PHP/LOGIC/SQL_connection.php';
+    include_once '/xampp/htdocs/DailyGreen-Project/SCRIPTS/PHP/LOGIC/functions.php';
     $sqlConnection = new SQLconnection();
     $admUserName = $sqlConnection->callTableBD('administrador',true);
-    
+    $usersArray = $sqlConnection->callTableBD('participante',true);
     function pullAdmName(){
         // pega o nome do administrador no arquivo .json
         $data = json_decode(file_get_contents("/xampp/htdocs/DailyGreen-Project/JSON/loginAdm.json"), true);
@@ -14,7 +15,7 @@
         $nomeAdministrador = $partsEmail[0];
         echo "Bem-vindo(a), " . $nomeAdministrador . "!";
     }
-    
+
 ?>
 
 <!DOCTYPE html>
@@ -44,7 +45,8 @@
             </div>
 
             <div class="menu_navegacao">
-                test
+                <div class="btn-listaAcc">Lista de contas</div>
+                more to come soon...
             </div>
 
             <div class="logout">
@@ -57,13 +59,26 @@
         <div class="menu_principal">
 
             <header class="header_menu_principal">
-                test
+                lista de usuarios
             </header>
-
             <div class="navegacao_principal">
-
+                <div class="lista-usuarios">
+                <?php foreach ($usersArray as $user): ?>
+                    <div class="user">
+                        <div class="user-avatar">
+                            <img src="<?= str_replace("/xampp/htdocs", "", htmlspecialchars($user['profile_pic'])) ?>"
+                                alt="Avatar" style="width: 100px; height: 100px; border-radius: 50%;">
+                        </div>
+                        <div class="user-info">
+                            <div class="username"> Username: <div class="user-name"><?= htmlspecialchars($user['username']) ?></div> </div>
+                            <div class="email"> Email: <div class="user-email"><?= htmlspecialchars($user['email']) ?></div> </div>
+                            <div class="create-time"> Creation time: <div class="user-creation-time"><?= htmlspecialchars($user['create_time']) ?></div> </div>
+                        </div>
+                    </div>
+                <?php endforeach; ?>
+                </div>
             </div>
-            
+
         </div>
 
     </div>
--- SCRIPTS/PHP/loginAdm.php
+++ SCRIPTS/PHP/loginAdm.php
@@ -1,8 +1,8 @@
+
 <?php
     $loginArray = json_decode(file_get_contents('/xampp/htdocs/DailyGreen-Project/JSON/loginAdm.json'),true);
     if (!$loginArray["find"]){
         echo "<script type='text/javascript'> alert('Email ou senha incorretos!') </script>";
     }
-    
-    include "/xampp/htdocs/DailyGreen-Project/SCRIPTS/HTML/tela_de_login_adm.html"; 
+    include_once "/xampp/htdocs/DailyGreen-Project/SCRIPTS/HTML/tela_de_login_adm.html";
 ?>
