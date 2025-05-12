92b84422cd2bb09bd87c6570040e98284037b3b6
Lucas Aioria Serpa <136469417+LucasAioriaSerpa@users.noreply.github.com>
2025-05-12 09:38:52 -0300

    small notation in SQL_connection.php

--- SCRIPTS/PHP/LOGIC/SQL_connection.php
+++ SCRIPTS/PHP/LOGIC/SQL_connection.php
@@ -1,6 +1,7 @@
 
 <?php
 include_once 'Cypher.php';
+session_start(); // ! REMOVE this 'session_start();' after the integration of all files JSON with SESSION
 class SQLconnection {
     private EncodeDecode $encodeDecode;
     private array $sqlData;
