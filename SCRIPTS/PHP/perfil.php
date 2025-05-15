153296d45b0d62ac65c82aea722c7949fba4c800
joao-au <augustojoao068@gmail.com>
2025-05-15 11:13:52 -0300

    Create perfil.php

---
+++ SCRIPTS/PHP/perfil.php
@@ -0,0 +1,15 @@
+<?php
+// Dados dinâmicos do perfil
+$usuario_nome = "participante1";
+$usuario_handle = "@participante1";
+$usuario_bio = "Technoking of Tesla, Imperator of Mars 🚀, Chief Twit";
+$usuario_foto = "https://pbs.twimg.com/profile_images/1683899100922511378/5lY42eHs_400x400.jpg";
+$usuario_capa = ""; // Insira o link da capa se quiser
+$usuario_tweets = "32.5 mil Tweets";
+$seguindo = "183";
+$seguidores = "181.9 milhões";
+
+// Inclui o HTML e insere as variáveis
+include 'perfil_template.html';
+?>
+
