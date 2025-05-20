bdc17bd326f3a911b1503192b0c21eaaa7718fd2
Flavia Cristina Fagundes <flaviacrisfagundes@gmail.com>
2025-05-20 12:17:49 -0300

    little changes

--- SCRIPTS/PHP/LOGIC/sendReport.php
+++ SCRIPTS/PHP/LOGIC/sendReport.php
@@ -3,4 +3,26 @@
     include_once 'SQL_connection.php';
     include_once 'functions.php';
     debug_var($_POST);
+
+    $sqlConnection = new SQLconnection();
+    $id_relator = $_POST['id_relator'];
+    $id_relatado = $_POST['id_relatado'];
+    $titulo = $_POST['titulo'];
+    $motivo = $_POST['motivo'];
+
+    $sqlQuery = "INSERT INTO denuncia(
+        id_relator,
+        id_relatado,
+        titulo,
+        motivo
+    ) VALUES (
+        '{$id_relator}',
+        '{$id_relatado}',
+        '{$titulo}',
+        '{$motivo}'
+    )";
+
+    $sqlConnection->insertQueryBD($sqlQuery);
+
+    header("Location: /DailyGreen-Project/SCRIPTS/PHP/postagens.php");
 ?>
