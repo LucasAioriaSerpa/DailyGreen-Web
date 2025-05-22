02c5c9830915875a3db77b6a436f8677cddb254b
Flavia Cristina Fagundes <flaviacrisfagundes@gmail.com>
2025-05-21 21:20:30 -0300

    update: buttons admPage

--- SCRIPTS/PHP/admPage.php
+++ SCRIPTS/PHP/admPage.php
@@ -54,16 +54,20 @@
         </div>
 
         <!-- MENU PRINCIPAL -->
-        <div class="menu_principal">
+        <div class="pagina_principal">
             <header class="header_menu_principal"></header>
             <div class="menu_principal" id="menu_principal" name="menu_principal">
                 <div class="button-row">
-                    <button class="btn-users-list" id="btn-users-list" name="btn-users-list">Lista de Usuários</button>
-                    <button class="btn-report-list" id="btn-report-list" name="btn-report-list">Lista de Denúncias</button>
+                    <div class="btn_users_report">
+                        <button class="btn-users-list" id="btn-users-list" name="btn-users-list" onclick="loadPage('/DailyGreen-Project/SCRIPTS/PHP/listUsers.php')">Lista de Usuários</button>
+                        <button class="btn-report-list" id="btn-report-list" name="btn-report-list" onclick="loadPage('/DailyGreen-Project/SCRIPTS/PHP/listReport.php')">Lista de Denúncias</button>
+                    </div>
                 </div>
                 <div class="button-row">
-                    <button class="btn-suspenso-list" id="btn-suspenso-list" name="btn-suspenso-list">Lista de Suspensos</button>
-                    <button class="btn-banido-list" id="btn-banido-list" name="btn-banido-list">Lista de Banidos</button>
+                    <div class="btn_suspenso_banido">
+                        <button class="btn-suspenso-list" id="btn-suspenso-list" name="btn-suspenso-list">Lista de Suspensos</button>
+                        <button class="btn-banido-list" id="btn-banido-list" name="btn-banido-list">Lista de Banidos</button>
+                    </div>
                 </div>
             </div>
         </div>
--- SCRIPTS/PHP/listButtonsAdm.php
+++ SCRIPTS/PHP/listButtonsAdm.php
@@ -8,15 +8,19 @@
     }
 ?>
 
-<div class="menu_principal">
+<div class="pagina_principal">
     <div class="menu_principal" id="menu_principal" name="menu_principal">
         <div class="button-row">
-            <button class="btn-users-list" id="btn-users-list" name="btn-users-list">Lista de Usuários</button>
-            <button class="btn-report-list" id="btn-report-list" name="btn-report-list">Lista de Denúncias</button>
+            <div class="btn_users_report">
+                <button class="btn-users-list" id="btn-users-list" name="btn-users-list" onclick="loadPage('/DailyGreen-Project/SCRIPTS/PHP/listUsers.php')">Lista de Usuários</button>
+                <button class="btn-report-list" id="btn-report-list" name="btn-report-list" onclick="loadPage('/DailyGreen-Project/SCRIPTS/PHP/listReport.php')">Lista de Denúncias</button>
+            </div>
         </div>
         <div class="button-row">
-            <button class="btn-suspenso-list" id="btn-suspenso-list" name="btn-suspenso-list">Lista de Suspensos</button>
-            <button class="btn-banido-list" id="btn-banido-list" name="btn-banido-list">Lista de Banidos</button>
+            <div class="btn_suspenso_banido">
+                <button class="btn-suspenso-list" id="btn-suspenso-list" name="btn-suspenso-list">Lista de Suspensos</button>
+                <button class="btn-banido-list" id="btn-banido-list" name="btn-banido-list">Lista de Banidos</button>
+            </div>
         </div>
     </div>
 </div>
