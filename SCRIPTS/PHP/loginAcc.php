4aeff9fcca69664f2ebbc81f8ed710a757019275
Nikolas Malinowski <nikis260606@gmail.com>
2025-05-27 11:02:53 -0300

    Aviso de login invalido colocado

--- SCRIPTS/PHP/loginAcc.php
+++ SCRIPTS/PHP/loginAcc.php
@@ -1,12 +1,26 @@
-
 <?php
 include_once 'LOGIC/session.php';
 include_once 'LOGIC/Cypher.php';
 include_once 'LOGIC/functions.php';
-if ($_SESSION['user']['find'] === false){
-    echo "<script type='text/javascript'> Swal.fire({title: 'Erro!', text: 'Email ou senha incorretos.', icon: 'error'}) </script>";
+if ($_SESSION['user']['find'] === false) {
     $_SESSION['user']['find'] = null;
+    
+    echo 
+    '<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    <script>
+    document.addEventListener("DOMContentLoaded", function() {
+        Swal.fire({
+            icon: "error",
+            title: "Login inválido",
+            text: "O login ou senha estão incorretos ou não existem."
+        }).then(() => {
+            window.location.href = "/DailyGreen-Project/SCRIPTS/PHP/loginAcc.php";
+        });
+    });
+    </script>';
+    exit();
 }
+
 include_once "/xampp/htdocs/DailyGreen-Project/SCRIPTS/HTML/pagina_login.html";
 ?>
 <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
